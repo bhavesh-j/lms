a7ec90c3a7ec10291cd04998d1a190a035d303e2
Anshu Patel <patelanshu85@gmail.com>
2020-12-05 03:40:25 +0530

    Monthly Installement

--- src/shared/baseurl.js
+++ src/shared/baseurl.js
@@ -1,11 +1,6 @@
 // production url
-const baseurl = 'https://bsms.dabinventive.com/';
+// const baseurl = 'https://bsms.dabinventive.com/';
 // local url
-<<<<<<< HEAD
-//const baseurl = 'https://localhost:44311/';
-=======
-// const baseurl = 'https://localhost:44311/';
->>>>>>> 7f9d1bac2d8c5a612b23837130f4f11e8fed2f3b
 
 const baseurl = 'https://bsms.dabinventive.com/';
 // local url
